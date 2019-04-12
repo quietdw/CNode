104193ef088a62b11f50d831250f988a4be83837
quietdw <1044569102@qq.com>
2019-04-12 20:59:54 +0800

    点击路由跳转

---
+++ src/components/list.vue
@@ -0,0 +1,17 @@
+<template>
+  <ul>
+    <li>
+      <router-link :to="{name:'HelloWorld',params:{hwmsg:'我是传来的数据'}}">HelloWorld</router-link>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: "List",
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="scss" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import List from '@/components/List'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
-    path: '/hello',
+    path: '/hello/:hwmsg',
     name: 'HelloWorld',
     component: HelloWorld
+  }, {
+    path: '/',
+    name: 'List',
+    component: List
   }]
 })
