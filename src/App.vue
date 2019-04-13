6798d78edd8bb2a576599e0f4db41d4b3a612b24
quietdw <1044569102@qq.com>
2019-04-14 00:11:59 +0800

    样式初始化

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,53 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <Header></Header>
+    <PostList></PostList>
   </div>
 </template>
 
 <script>
+import Header from "./components/Header";
+import PostList from "./components/PostList";
+
 export default {
-  name: "App"
+  name: "App",
+  components: {
+    Header,
+    PostList
+  }
 };
 </script>
 
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+}
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  font-weight: 400;
+}
+li {
+  list-style-type: none;
+}
+body {
+  background: #e1e1e1;
+  font-family: "Helvetica Neue", "Luxi Sans", "DejaVu Sans", Tahoma,
+    "Hiragino Sans GB", STHeiti, sans-serif;
+}
+a {
+  color: inherit;
+  text-decoration: none;
+}
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
 }
 </style>
