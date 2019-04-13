bd7ff2f0ee78243bdb5192dfe0bd3bdf6b21abc2
quietdw <1044569102@qq.com>
2019-04-14 00:11:45 +0800

    使用utils和组件

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import request from "@/helpers/request.js";
+import utils from "@/helpers/utils.js"
 
 Vue.config.productionTip = false
 
-
-
+Vue.prototype.request = request
+Vue.use(utils)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import List from '@/components/List'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [{
-    path: '/hello/:hwmsg',
-    name: 'HelloWorld',
-    component: HelloWorld
-  }, {
-    path: '/',
-    name: 'List',
-    component: List
-  }]
+  routes: []
 })
