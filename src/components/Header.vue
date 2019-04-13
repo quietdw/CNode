dcb2ef3aaa94ab024091b02a21209b6f2488a799
quietdw <1044569102@qq.com>
2019-04-14 00:10:06 +0800

    页面头部

---
+++ src/components/Header.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="header">
+    <div class="inner-container">
+      <div class="container">
+        <svg class="icon" aria-hidden="true">
+          <use xlink:href="#n-cnode"></use>
+        </svg>
+        <ul>
+          <li>
+            <a href="#">首页</a>
+          </li>
+          <li>
+            <a>新手入门</a>
+          </li>
+          <li>
+            <a>API</a>
+          </li>
+          <li>
+            <a>关于</a>
+          </li>
+          <li>
+            <a>注册</a>
+          </li>
+          <li>
+            <a>登陆</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$background: #444;
+
+.header {
+  font-size: 13px;
+  background: $background;
+  width: 100%;
+  > .inner-container {
+    width: 90%;
+    margin: 0 auto;
+    padding: 10px 5px 12px 5px;
+    display: flex;
+    .container {
+      width: 100%;
+      min-width: 960px;
+      max-width: 1400px;
+      margin: 0 auto;
+      display: flex;
+      justify-content: space-between;
+      > svg {
+        width: 9.230769em;
+        height: 2.153846em;
+      }
+      > ul {
+        display: flex;
+        align-items: center;
+        > li {
+          padding: 0 15px;
+          color: #ccc;
+          > a {
+            &:hover {
+              color: #fff;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+</style>
