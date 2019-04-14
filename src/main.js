577ceaf31f9094f4292f0fcca6a04d2e3c1d2204
quietdw <1044569102@qq.com>
2019-04-15 02:26:47 +0800

    安装使用 element ui

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import router from './router'
 import request from "@/helpers/request.js";
 import utils from "@/helpers/utils.js"
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 Vue.prototype.request = request
@@ -19,10 +21,12 @@ Vue.filter('newTag', (value, data) => {
 })
 
 Vue.use(utils)
+Vue.use(ElementUI);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  render: h => h(App),
   components: {
     App
   },
