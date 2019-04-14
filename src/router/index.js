fe305c8118dd910ef0a9c8c16d3e5cce9a9f5612
quietdw <1044569102@qq.com>
2019-04-14 15:24:09 +0800

    增加index、article路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import PostList from "../components/PostList";
+import Article from "../components/Article";
+
 Vue.use(Router)
 
 export default new Router({
-  routes: []
+  routes: [{
+      name: 'article',
+      path: '/article/:id',
+      component: Article
+    },
+    {
+      name: 'index',
+      path: '/',
+      component: PostList
+    }
+  ]
 })
